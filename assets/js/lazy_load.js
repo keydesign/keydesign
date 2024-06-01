9828b17b9eb85b77c663db962fe18fe18ec484e1
Aaron Campbell <aaron@keydesign.com>
2024-06-01 22:14:29 +0300

    Disable and remove low resolution lazy-loading until the low resolution images can be automated.

--- assets/js/lazy_load.js
+++ assets/js/lazy_load.js
@@ -1,32 +1,32 @@
-function lazy_load() {
-    // Select all images with the class 'lazy-load'
-    const lazyImages = document.querySelectorAll('.lazy-load');
+// function lazy_load() {
+//     // Select all images with the class 'lazy-load'
+//     const lazyImages = document.querySelectorAll('.lazy-load');
 
-    lazyImages.forEach(img => {
-        // Add a small delay (e.g., 50ms) to give the full size image a chance to load
-        setTimeout(() => {
-            // Check if the image has not been loaded yet
-            if (!img.complete) {
-                // Store the original source of each image
-                const originalSrc = img.src;
+//     lazyImages.forEach(img => {
+//         // Add a small delay (e.g., 50ms) to give the full size image a chance to load
+//         setTimeout(() => {
+//             // Check if the image has not been loaded yet
+//             if (!img.complete) {
+//                 // Store the original source of each image
+//                 const originalSrc = img.src;
 
-                // Modify the source temporarily
-                img.src = originalSrc.replace(/\.(?=[^.]*$)/, '_lr.');
+//                 // Modify the source temporarily
+//                 img.src = originalSrc.replace(/\.(?=[^.]*$)/, '_lr.');
 
-                // Apply a blur effect to the low-resolution image
-                img.style.filter = 'blur(5px)'; // You can adjust the blur radius as needed
+//                 // Apply a blur effect to the low-resolution image
+//                 img.style.filter = 'blur(5px)'; // You can adjust the blur radius as needed
 
-                // Add an event listener to detect when the temporary image has loaded
-                img.addEventListener('load', () => {
-                    // Remove the blur effect
-                    img.style.filter = 'none';
+//                 // Add an event listener to detect when the temporary image has loaded
+//                 img.addEventListener('load', () => {
+//                     // Remove the blur effect
+//                     img.style.filter = 'none';
 
-                    // Replace the source of the image back to the original source
-                    img.src = originalSrc;
-                });
-            }
-        }, 100);
-    });
-}
+//                     // Replace the source of the image back to the original source
+//                     img.src = originalSrc;
+//                 });
+//             }
+//         }, 100);
+//     });
+// }
 
-document.addEventListener('DOMContentLoaded', lazy_load());
+// document.addEventListener('DOMContentLoaded', lazy_load());
